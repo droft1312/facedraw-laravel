f83260e25946a550030749406f04be72e41ca1cc
Turar Jumaniyaz <droft1312@gmail.com>
2018-11-25 15:21:50 +0100

    added navbar

--- drawface-app/resources/views/pages/index.blade.php
+++ drawface-app/resources/views/pages/index.blade.php
@@ -1,1 +1,7 @@
-<h1>Hello</h1>
+@extends('layouts.app')
+@section('content')
+  <div class="jumbotron text-center">
+    <h1 class="jumbotron-heading">Welcome to FaceDraw!</h1>
+    <p> <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a> </p>
+  </div>﻿
+@endsection
