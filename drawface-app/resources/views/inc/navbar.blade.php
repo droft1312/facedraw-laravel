77342fd65fbc4ee7d1856d0397737a7b0fa8b8a8
Turar Jumaniyaz <droft1312@gmail.com>
2018-11-25 16:13:12 +0100

    did some styling

--- drawface-app/resources/views/inc/navbar.blade.php
+++ drawface-app/resources/views/inc/navbar.blade.php
@@ -10,10 +10,7 @@
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/services">Services</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
+                <a class="nav-link" href="/about">About us</a>
             </li>
         </ul>
     </div>
--- drawface-app/resources/views/pages/index.blade.php
+++ drawface-app/resources/views/pages/index.blade.php
@@ -2,7 +2,8 @@
 @section('content')
 <div class="jumbotron text-center">
     <h1 class="jumbotron-heading">Welcome to FaceDraw!</h1>
+    <p class="lead text-muted">By using this app you'll be able to see your pictures drawn with text!</p>
     <p> <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
-      <a class="btn btn-success btn-lg" href="/register" role="button">Register</a> </p>
+        <a class="btn btn-success btn-lg" href="/register" role="button">Register</a> </p>
 </div>﻿
 @endsection
