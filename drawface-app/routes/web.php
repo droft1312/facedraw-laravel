269d8139e0abcb8af3fa004e14b45f2e45ede2fe
Turar Jumaniyaz <droft1312@gmail.com>
2018-11-26 13:30:16 +0100

    added tables and stuff

--- drawface-app/routes/web.php
+++ drawface-app/routes/web.php
@@ -3,3 +3,5 @@
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/login', 'PagesController@login');
+
+Route::resource('messages', 'MessagesController');
